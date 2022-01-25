96a368c27ee3c42bbaac742fa39d5b95592c8e0e
zDany01 <85403430+zDany01@users.noreply.github.com>
2022-01-25 13:12:44 +0100

    Setting up mod configs...

--- src/main/java/io/github/zdany01/respawntotem/MainClass.java
+++ src/main/java/io/github/zdany01/respawntotem/MainClass.java
@@ -1,10 +1,18 @@
 package io.github.zdany01.respawntotem;
 
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
 
 @Mod("respawntotem")
 public class MainClass {
     public static final String MOD_ID = "respawntotem";
+    
+    private static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, MOD_ID);
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MOD_ID);
+
     public MainClass(){
         
     }
