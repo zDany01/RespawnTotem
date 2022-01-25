c76611fd6258429d0ae45745fc8ef0ff596ebff8
zDany01 <85403430+zDany01@users.noreply.github.com>
2022-01-25 12:47:08 +0100

    Initial Push

---
+++ src/main/java/io/github/zdany01/respawntotem/MainClass.java
@@ -0,0 +1,11 @@
+package io.github.zdany01.respawntotem;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod("respawntotem")
+public class MainClass {
+    public static final String MOD_ID = "respawntotem";
+    public MainClass(){
+        
+    }
+}
