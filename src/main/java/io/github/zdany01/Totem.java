af502bf1c41637f8d6febca8e5cf0b92d737d44c
zDany01 <85403430+zDany01@users.noreply.github.com>
2022-01-26 15:47:01 +0100

    Added recipe Added Hardcore setup function(for my server) Fixed Texture

--- src/main/java/io/github/zdany01/Totem.java
+++ src/main/java/io/github/zdany01/Totem.java
@@ -1,13 +1,32 @@
 package io.github.zdany01;
 
+import com.google.errorprone.annotations.OverridingMethodsMustInvokeSuper;
+
 import io.github.zdany01.respawntotem.MainClass;
+import net.minecraft.client.Minecraft;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.client.gui.Gui;
+import net.minecraft.client.gui.GuiComponent;
+import net.minecraft.client.gui.chat.*;
 
 public class Totem extends Item{
     public Totem(){
         super(new Item.Properties()
         .tab(MainClass.RespawnTotemCTab)
-        
+
         );
     }
+
+    public InteractionResultHolder<ItemStack> use(Level world, Player player, InteractionHand hand){
+        
+        return super.use(world,player,hand);
+    }
+
+
 }
